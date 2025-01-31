e24a6d598a44c7c3d6f2500c1efb7837cfeb084a
UmMochileiro <matheuscardoso86@hotmail.com>
2025-01-31 17:58:06 -0300

    Add files via upload

---
+++ Aula 1/funcoes.js
@@ -0,0 +1,35 @@
+
+// Primeira saudação - Apenas a função
+
+function saudacao () {
+
+    console.log("Olá Mundo!");
+};
+
+saudacao();
+
+
+// Segunda saudação - Agora com o argumento nome
+
+function novaSaudacao (nome) {
+
+    console.log(`Olá ${nome}, Seja bem-vindo(a)!`);
+
+};
+
+novaSaudacao("Matheus");
+novaSaudacao("Guilherme");
+novaSaudacao("Tais");
+
+
+// Agora, a função de soma
+
+function soma (a,b) {
+
+return resultado = a+b;
+
+};
+
+console.log(soma(1,2));
+console.log(soma(3,4));
+console.log(soma(5,6));
