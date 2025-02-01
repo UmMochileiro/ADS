aa01258d687b06c726bdb75d9315acf22317e341
UmMochileiro <matheuscardoso86@hotmail.com>
2025-01-31 22:23:03 -0300

    Add files via upload

---
+++ Operadores de atribuição/Operadoresatribuicao.js
@@ -0,0 +1,33 @@
+
+
+// +=
+// -=
+// /=
+// *=
+// %=
+// **=
+
+let a = 10;
+console.log(`valor inicial de a: ${a}`)
+console.log("\n")
+
+a += 10;
+console.log(`valor de a depois de += com 10: ${a}`)
+a -= 5;
+console.log(`valor de a depois de -= com 5: ${a}`)
+a /= 1;
+console.log(`valor de a depois de / com 1: ${a}`)
+a *= 1;
+console.log(`valor de a depois de *com 1: ${a}`)
+a **= 2;
+console.log(`valor de a depois de ** com 2: ${a}`)
+a %= 2;
+console.log(`valor de a depois de %= com 2: ${a}`)
+a *= 5;
+console.log(`valor de a depois de multiplicar por 5 =: ${a}`)
+a *= 2;
+console.log(`valor de a depois de multiplicar por 2 =: ${a}`)
+
+
+
+
