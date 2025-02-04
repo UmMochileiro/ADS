2f343a19bd2ae8eaf57969a1d1c4549072f42929
UmMochileiro <matheuscardoso86@hotmail.com>
2025-02-04 15:43:10 -0300

    Add files via upload

---
+++ Tolower touppercase/upperandlowercase.js
@@ -0,0 +1,8 @@
+let Nome = "Matheus Cardoso";
+console.log("Nome Inicial: ", Nome);
+
+let Maior = Nome.toUpperCase();
+let Menor = Nome.toLowerCase();
+
+console.log("Nome em Maiusculo: ", Maior);
+console.log("Nome em Minusculo: ", Menor);
